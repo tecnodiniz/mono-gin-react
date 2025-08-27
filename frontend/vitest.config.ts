45623466bca18bd599745ff4eee21c5530396e33
Eduardo Diniz <diniz480@gmail.com>
2025-08-27 15:17:02 -0300

    feat: add test to services/users

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/config/setup.ts",
